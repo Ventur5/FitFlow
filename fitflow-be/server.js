d319b423a3484840e572baf014c5273b6515bbb7
Valerio Ventura <valerioventura229@gmail.com>
2026-02-27 15:57:37 +0100

    fix13.0

--- fitflow-be/server.js
+++ fitflow-be/server.js
@@ -19,12 +19,12 @@ const app = express();
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  const isVercel = origin && (origin.endsWith('.vercel.app') || origin === 'https://fit-flow-fe.vercel.app');
-  
-  if (isVercel) {
+
+  if (origin && (origin.endsWith('.vercel.app') || origin === 'http://localhost:5173')) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
